7141a40e871a013bad1b89a2d3e6c28e98747a3c
Livia Emilie Valenti Stokke <livia.e.v.stokke@ntnu.no>
2021-09-27 00:48:29 +0200

    perf: target specific attributes for transition #4

--- src/global.tsx
+++ src/global.tsx
@@ -24,7 +24,7 @@ body {
     background: ${({ theme }) => theme.body};
     color: ${({ theme }) => theme.text};
     font-family: BlinkMacSystemFont, -apple-system, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif;
-    transition: all 0.25s linear;
+    transition: background 0.3s linear, color 0.3s linear;
 }
 `;
 
