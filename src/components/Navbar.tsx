08bc11ed367e8ca255bfe6f1b3c7254e56125c49
Livia Emilie Valenti Stokke <livia.e.v.stokke@ntnu.no>
2021-09-27 00:47:50 +0200

    feat: minimal navbar styling #4

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,12 +3,22 @@ import ThemeToggle from "./Theme/ThemeToggle";
 
 const StyledNav = styled.nav`
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
   width: 100%;
-  padding: 10px;
   box-shadow: ${({ theme }) => theme.shadow};
-  color: ${({ theme }) => theme.text};
-  background-color: ${({ theme }) => theme.card};
+  color: ${({ theme }) => theme.primary};
+  background: ${({ theme }) => theme.card};
+  transition: background 0.3s linear, color 0.3s linear;
+`;
+
+const NavContentWrapper = styled.span`
+  display: flex;
+  justify-content: space-between;
+  padding: 20px;
+  width: 60%;
+  @media (max-width: 1100px) {
+    width: 100%;
+  }
 `;
 
 const Title = styled.h1`
@@ -18,8 +28,10 @@ const Title = styled.h1`
 function Navbar() {
   return (
     <StyledNav>
-      <Title>Gitlab mini-dashboard</Title>
-      <ThemeToggle />
+      <NavContentWrapper>
+        <Title>Gitlab mini-dashboard</Title>
+        <ThemeToggle />
+      </NavContentWrapper>
     </StyledNav>
   );
 }
--- src/components/Theme/ThemeToggle.tsx
+++ src/components/Theme/ThemeToggle.tsx
@@ -1,6 +1,28 @@
+import styled from "styled-components";
+import { SunFill } from "@styled-icons/bootstrap/SunFill";
+import { Sun } from "@styled-icons/bootstrap/Sun";
 import { useContext } from "react";
 import { ThemeContext } from "./ThemeProvider";
 import ThemeTypes from "../../utils/themeutils";
+
+const StyledButton = styled.button`
+  border: none;
+  text-decoration: none;
+  background-color: ${({ theme }) => theme.primary};
+  border-radius: 7px;
+  padding: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-shadow: ${({ theme }) => theme.shadow};
+  & * {
+    fill: white;
+  }
+  &:hover {
+    background-color: ${({ theme }) => theme.primaryDark};
+  }
+  transition: background-color 0.1s linear;
+`;
 
 const ThemeToggle = () => {
   const { theme, setTheme } = useContext(ThemeContext);
@@ -11,9 +33,13 @@ const ThemeToggle = () => {
       : setTheme(ThemeTypes.LIGHT);
 
   return (
-    <button onClick={toggleTheme} aria-label="Toggle color-mode" type="button">
-      Toggle
-    </button>
+    <StyledButton
+      onClick={toggleTheme}
+      aria-label="Toggle color-mode"
+      type="button"
+    >
+      {theme === ThemeTypes.LIGHT ? <SunFill size={28} /> : <Sun size={28} />}
+    </StyledButton>
   );
 };
 
