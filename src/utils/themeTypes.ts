15b41b1742a34cdd67d8da0dd72f54522e3f0ccd
Livia Emilie Valenti Stokke <livia.e.v.stokke@ntnu.no>
2021-09-28 14:25:09 +0200

    docs: comment eslint-disable reason #4

--- src/utils/themeTypes.ts
+++ src/utils/themeTypes.ts
@@ -1,6 +1,8 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-shadow */
-export enum ThemeTypes {
+/* NB! Disabled because of eslint bug NB! */
+/* https://github.com/typescript-eslint/typescript-eslint/blob/master/docs/getting-started/linting/FAQ.md#i-am-using-a-rule-from-eslint-core-and-it-doesnt-work-correctly-with-typescript-code */
+enum ThemeTypes {
   DARK = "dark",
   LIGHT = "light",
 }
