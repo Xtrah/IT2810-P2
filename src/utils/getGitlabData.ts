fa3297b1bdff1d8bb35c4d6dfd43701eb9124c46
Marcus Haaland <marcuhaa@stud.ntnu.no>
2021-09-29 10:26:21 +0200

    fix: increase fetching per page #13

--- src/utils/getGitlabData.ts
+++ src/utils/getGitlabData.ts
@@ -3,13 +3,16 @@
 // https://piazza.com/class/ksk8rtnewz56sh?cid=48
 const getGitlabData = async (apiPath: string) => {
   const projectUrl = `https://gitlab.stud.idi.ntnu.no/api/v4/projects/${process.env.REACT_APP_GITLAB_PROJECT_ID}`;
-  const response = await fetch(projectUrl + apiPath, {
-    method: "GET",
-    headers: new Headers({
-      Authorization: `Bearer ${process.env.REACT_APP_GITLAB_PROJECT_ACCESS_TOKEN}`,
-      "Content-Type": "application/json",
-    }),
-  });
+  const response = await fetch(
+    `${projectUrl}${apiPath}?pagination=keyset&per_page=100`,
+    {
+      method: "GET",
+      headers: new Headers({
+        Authorization: `Bearer ${process.env.REACT_APP_GITLAB_PROJECT_ACCESS_TOKEN}`,
+        "Content-Type": "application/json",
+      }),
+    }
+  );
 
   return response.json();
 };
